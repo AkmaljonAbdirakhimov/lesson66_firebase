544212ba69ab832425c585d0f87372f851b11f9a
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-06-28 12:32:35 +0500

    firebase firestore

--- lib/controllers/products_controller.dart
+++ lib/controllers/products_controller.dart
@@ -1,3 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lesson66_firebase/services/products_firebase_services.dart';
 
-class ProductsController extends ChangeNotifier {}
+class ProductsController extends ChangeNotifier {
+  final _productsFirebaseServices = ProductsFirebaseServices();
+
+  Stream<QuerySnapshot> get list {
+    return _productsFirebaseServices.getProducts();
+  }
+
+  void addProduct(String title, double price) {
+    _productsFirebaseServices.addProduct(title, price);
+  }
+
+  void editProduct(String id, String title) {
+    _productsFirebaseServices.editProduct(id, title);
+  }
+}
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,11 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Product {
   String id;
   String title;
-  double price;
+  num price;
 
   Product({
     required this.id,
     required this.title,
     required this.price,
   });
+
+  factory Product.fromJson(QueryDocumentSnapshot query) {
+    return Product(
+      id: query.id,
+      title: query['title'],
+      price: query['price'],
+    );
+  }
 }
